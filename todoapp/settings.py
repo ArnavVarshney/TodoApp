4dcfa817bd9fb8cf2c1d13c428a4ee31e8d3e47c
ArnavVarshney <varshney.arnav@gmail.com>
2019-12-22 17:16:15 +0530

    Add Heroku link to ALLOWED_HOSTS
    Signed-off-by: Arnav Varshney <varshney.arnav@gmail.com>

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'your secret key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://todoappp29.herokuapp.com']
 
 # Application definition
 
