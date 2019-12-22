1c523400b57b6fc38c9cb963393fea1bfee3bf83
ArnavVarshney <varshney.arnav@gmail.com>
2019-12-22 22:47:36 +0530

    Fix defaults + Polishing
    Signed-off-by: Arnav Varshney <varshney.arnav@gmail.com>

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'your secret key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todoappp29.herokuapp.com', 'todoappp29.herokuapp.com', 'ghartodo.cf']
+ALLOWED_HOSTS = ['127.0.0.1', 'todoappp29.herokuapp.com', 'ghartodo.cf']
 
 # Application definition
 
